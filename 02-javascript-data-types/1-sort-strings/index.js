f6ad02cd74bfad8e50b9c6af4b1c12f16ab72d80
Yuri <34709153+y-dubovitsky@users.noreply.github.com>
2021-03-09 23:12:43 +0300

    2. JS Data-types. 1-Sort strings via locale.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,28 @@
 /**
- * sortStrings - sorts array of string by two criteria "asc" or "desc"
+ * sortStrings - sorts array of string by two criteria 'asc' or 'desc'
  * @param {string[]} arr - the array of strings
- * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+ * @param {string} [param='asc'] param - the sorting type 'asc' or 'desc'
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let result = new Array(...arr);
+    result = result.sort(
+        (a, b) => compareViaLocales(a, b, ['ru', 'en'])
+    );
 
+    return param === 'asc' ? result : result.reverse();
+}
+
+function compareViaLocales(a, b, locales) {
+    const collator = new Intl.Collator(
+        [
+            ...locales
+        ],
+        {
+            sensitivity: 'variant',
+            caseFirst: 'upper'
+        }
+    );
+
+    return collator.compare(a, b);
 }
