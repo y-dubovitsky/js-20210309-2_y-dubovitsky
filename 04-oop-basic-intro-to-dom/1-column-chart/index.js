d5e987671cc979ab0ebc758a9d6da96b69200692
user <y.dubovitsky@gmail.com>
2021-03-24 19:36:00 +0300

    04-oop-basic-intro Unfinished Solution.

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,53 @@
 export default class ColumnChart {
 
+  constructor(object = {}) {
+      this.data = object.data;
+      this.label = object.label;
+      this.link = object.link;
+      this.value = object.value;
+      this.chartHeight = 50;
+      this.render();
+  }
+
+  update = (newData) => {
+    this.data = newData;
+    const columnChartChart = this.element.querySelector('.column-chart__chart');
+    columnChartChart.innerHTML = this.createBody();
+  }
+
+  createBody = () => {
+    if(!this.data || this.data.length === 0) return `<img src="./charts-skeleton.svg">`
+    return this.data.reduce((prev, cur) => {
+      return prev + `<div style="--value: ${cur}" data-tooltip="${cur}%"></div>`
+    }, ``)
+  }
+
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = `
+        <div class="column-chart ${!this.data || this.data.length === 0 ? 'column-chart_loading' : ''} " style="--chart-height: ${this.chartHeight}">
+          <div class="column-chart__title">
+            Total ${this.label}
+            ${this.link ? `<a href="/${this.link}" class="column-chart__link">View all</a>` : ``}
+          </div>
+          <div class="column-chart__container">
+            <div data-element="header" class="column-chart__header">${this.value}</div>
+            <div data-element="body" class="column-chart__chart">
+              ${this.createBody()}
+            </div>
+          </div>
+        </div>
+        `
+
+    this.element = element.firstElementChild;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
