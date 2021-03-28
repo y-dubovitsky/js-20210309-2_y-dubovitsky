3e9302797197aec371687841c2b76cdbc5194859
Yuri <34709153+y-dubovitsky@users.noreply.github.com>
2021-03-28 20:15:58 +0300

    Temporary solution of 05-dom-document-loading.

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,60 @@
 export default class NotificationMessage {
 
+    static instance;
+
+    constructor(
+        message = '',
+        {
+            duration = '',
+            type = ''
+        } = {}
+    ) {
+        if (NotificationMessage.instance) {
+            return NotificationMessage.instance;
+        }
+        NotificationMessage.instance = this;
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+    }
+
+    get template() {
+        return `
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = this.template;
+        this.element = element.firstElementChild;
+
+        document.body.append(this.element);
+    }
+
+    show() {
+        if (!this.element) {
+            this.render();
+            setTimeout(() => {
+                this.destroy();
+            }, this.duration)
+        }
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        NotificationMessage.instance = null;
+    }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,120 @@
 export default class SortableTable {
 
+  constructor(
+    header = [],
+    { data } = [],
+  ) {
+    this.header = header;
+    this.data = data;
+    this.render();
+  }
+
+  getSortArrow(sortable) {
+    if (sortable) {
+      return `
+              <span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+              </span>
+            `
+    }
+    return ''
+  }
+
+  getSortableHeader() { // this.header - вынести в сигнатуру метода?
+    const result = this.header.map(({ id, title, sortable, template, sortType }) => {
+      return `
+              <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-sort-type="${sortType}" data-order="asc">
+                <span>${title}</span>
+                ${this.getSortArrow(sortable)}
+              </div>
+              `
+    }).join('');
+
+    return `
+            <div data-element="header" class="sortable-table__header sortable-table__row">
+              ${result}
+            </div>
+          `
+  }
+
+  getSortableTableBody(sortField = 'title', orderValue = 'asc') { //this.data - вынести в сигнатуру метода?
+    const result = this.sortDataByParam(this.data, sortField, orderValue) // title - by default
+      .map(({ id, title, description, quantity, subcategory, status, images, price, discount, sales }) => {
+        return `
+            <a href="${images?.[0]?.url}" class="sortable-table__row">
+              <div class="sortable-table__cell">
+                <img class="sortable-table-image" alt="Image" src="${images?.[0]?.url}">
+              </div>
+              <div class="sortable-table__cell">${title}</div>
+
+              <div class="sortable-table__cell">${quantity}</div>
+              <div class="sortable-table__cell">${price}</div>
+              <div class="sortable-table__cell">${sales}</div>
+            </a>
+            `
+      }).join('');
+
+    return result;
+  }
+
+  get template() {
+    return `
+        <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+      
+          ${this.getSortableHeader()}
+      
+          <div data-element="body" class="sortable-table__body">
+            ${this.getSortableTableBody()}
+          </div>
+      
+          <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+      
+          <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+            <div>
+              <p>No products satisfies your filter criteria</p>
+              <button type="button" class="button-primary-outline">Reset all filters</button>
+            </div>
+          </div>
+      
+        </div>
+      </div>
+        `
+  }
+
+  sortDataByParam(arrOfObjects, sortField, orderValue) {
+
+    function compareObjects(a, b, sortField, orderValue) {
+      const direction = orderValue === 'asc' ? 1 : -1;
+      const comparedFieldA = a[sortField];
+      const comparedFieldB = b[sortField];
+
+      let comparison = 0;
+
+      if (comparedFieldA > comparedFieldB) {
+        comparison = 1;
+      } else if (comparedFieldA < comparedFieldB) {
+        comparison = -1;
+      }
+      return comparison * direction;
+    }
+
+    return arrOfObjects.sort((a, b) => compareObjects(a, b, sortField, orderValue));
+  }
+
+  sort(sortField, orderValue) {
+    const updatedTableBody = this.getSortableTableBody(sortField, orderValue);
+    this.sortableTableBody.innerHTML = updatedTableBody;
+  }
+
+  render() {
+    const tempWrapper = document.createElement('div');
+    tempWrapper.innerHTML = this.template;
+    this.element = tempWrapper.firstElementChild;
+
+    this.sortableTableBody = this.element.querySelector('[data-element="body"]'); //TODO  Вынести data-elemetns
+  }
+
+
 }
 
