4a8018479c3ba2f6a78b4b780c1d371789728f22
user <y.dubovitsky@gmail.com>
2021-04-02 11:21:22 +0300

    06-events-practice.

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,3 +1,183 @@
 export default class SortableTable {
 
+    constructor(
+        header = [],
+        { data } = [],
+        compareViaIntlCollarator = {}
+    ) {
+        this.header = header;
+        this.data = data;
+        this.compareViaIntlCollarator = compareViaIntlCollarator;
+        this.render();
+    }
+
+    getSortArrow(sortable) {
+        return sortable ?
+            `
+          <span data-element="arrow" class="sortable-table__sort-arrow">
+            <span class="sort-arrow"></span>
+          </span>
+        `
+            :
+            ''
+    }
+
+    getSortableHeader(header = []) {
+        const result = header.map(({ id, title, sortable, template, sortType }) => {
+            return `
+                <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-sort-type="${sortType}" data-order="asc">
+                  <span>${title}</span>
+                  ${this.getSortArrow(sortable)}
+                </div>
+                `
+        }).join('');
+
+        return `
+              <div data-element="header" class="sortable-table__header sortable-table__row">
+                ${result}
+              </div>
+            `
+    }
+
+    getSortableTableBody(data = []) {
+        const result = data.map(({ title, quantity, images, price, sales }) => {
+            return `
+              <a href="${images?.[0]?.url}" class="sortable-table__row">
+                <div class="sortable-table__cell">
+                  <img class="sortable-table-image" alt="Image" src="${images?.[0]?.url}">
+                </div>
+                <div class="sortable-table__cell">${title}</div>
+  
+                <div class="sortable-table__cell">${quantity}</div>
+                <div class="sortable-table__cell">${price}</div>
+                <div class="sortable-table__cell">${sales}</div>
+              </a>
+              `
+        }).join('');
+
+        return result;
+    }
+
+    get template() {
+        return `
+          <div data-element="productsContainer" class="products-list__container">
+          <div class="sortable-table">
+            ${this.getSortableHeader(this.header)}
+            <div data-element="body" class="sortable-table__body">
+              ${this.getSortableTableBody(this.data)}
+            </div>
+        
+            <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
+        
+            <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
+              <div>
+                <p>No products satisfies your filter criteria</p>
+                <button type="button" class="button-primary-outline">Reset all filters</button>
+              </div>
+            </div>
+        
+          </div>
+        </div>
+          `
+    }
+
+    render() {
+        const tempWrapper = document.createElement('div');
+        tempWrapper.innerHTML = this.template;
+        this.element = tempWrapper.firstElementChild;
+
+        this.sortableTableBody = this.element.querySelector('[data-element="body"]'); //Одного data-elemeta достаточно
+        this.headerElements = this.element.querySelectorAll('.sortable-table__header .sortable-table__cell'); //FIXME Улучшить?
+        this.onClickTableHeaderDataSort();
+    }
+
+    sort(sortField, direction) {
+        const sortedData = this.sortDataByParam(this.data, sortField, direction); // sort data
+        const updatedTableBody = this.getSortableTableBody(sortedData);
+        this.sortableTableBody.innerHTML = updatedTableBody;
+    }
+
+    // Event Listeners
+    onClickTableHeaderDataSort() {
+        this.headerElements.forEach(element => {
+            const { sortable } = element.dataset;
+            if (sortable) {
+                element.addEventListener('click', () => {
+                    const { id, order } = element.dataset;
+                    this.sort(id, order);
+                    changeSortDirection(element, order);
+                })
+            }
+        })
+
+        // local function
+        function changeSortDirection(element, order) {
+            element.dataset.order = (order === 'asc' ? 'desc' : (order === 'desc' ? 'asc' : ''))
+            // switch (order) {
+            //     case 'asc': {
+            //         element.dataset.order = 'desc';
+            //         break;
+            //     }
+            //     case 'desc': {
+            //         element.dataset.order = 'asc'
+            //     }
+            // }
+        }
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
+
+    /**
+     * Sorting array of objects
+     * @returns sorted array of objects
+     */
+    sortDataByParam(arrOfObjects = [], sortField, sortDirection) {
+        return arrOfObjects.sort((a, b) => this.compareObjects(a, b, sortField, sortDirection));
+    }
+
+    compareObjects(a, b, sortField, sortDirection) {
+        let compareResult = 0;
+        const fieldA = a[sortField];
+        const fieldB = b[sortField];
+
+        const direction = sortDirection === 'asc' ? 1 : sortDirection === 'desc' ? -1 : '' //* Вычисляем коэффициент направления сортировки
+
+        if (typeof fieldA === 'string') {
+            compareResult = this.compareViaIntlCollarator(fieldA, fieldB, ['ru', 'en'], direction);
+        }
+        if (typeof fieldB === 'number') {
+            compareResult = compareNumber(fieldA, fieldB, direction);
+        }
+
+        return compareResult;
+
+        // local function
+        function compareNumber(a, b, direction) {
+            let result = 0;
+
+            result = a > b ? direction * 1 : direction * -1;
+            return result;
+        }
+    }
+
+    /**
+    * Return array of uniq fields in this.header
+    */
+    getHeaderUniqFields() {
+        const uniqFields = [];
+
+        this.header.forEach(object => {
+            Object.keys(object).forEach(key => {
+                !result.includes(key) ? result.push(key) : ''
+            })
+        })
+
+        return uniqFields;
+    }
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,60 @@
 class Tooltip {
 
+    constructor() {
+        if (!Tooltip._instance) {
+            Tooltip._instance = this;
+        }
+        return Tooltip._instance;
+    }
+
+    getTemplate() {
+        return `<div class="tooltip">${this.message}</div>`
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = this.getTemplate();
+        this.element = element;
+        document.body.appendChild(this.element);
+    }
+
+    addToolTip = (element) => {
+        this.message = element.getAttribute('data-tooltip');
+        this.render();
+    }
+
+    showTooltip(event) {
+        switch (event.target.dataset.tooltip) {
+            case "bar-bar-bar": {
+                this.message = "bar-bar-bar";
+                this.render();
+                event.stopPropagation();
+                break;
+            }
+            case "foo": {
+                this.message = "foo";
+                this.render();
+                break;
+            }
+        }
+    }
+
+    initialize() {
+        const dataToolTips = document.querySelectorAll('[data-tooltip]');
+
+        dataToolTips.forEach(tip => {
+            tip.addEventListener('pointerover', event => {
+                this.showTooltip(event);
+            })
+            tip.addEventListener('pointerout', () => {
+                this.remove();
+            })
+        })
+    }
+
+    remove() {
+        this.element.remove();
+    }
 }
 
 const tooltip = new Tooltip();
